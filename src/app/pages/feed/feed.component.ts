df1eb536144b26e9c16bb83615e8cfaef7811149
Diego Bloise <diego-dev@outlook.com>
2025-07-14 15:56:26 -0300

    update: event card

--- src/app/pages/feed/feed.component.ts
+++ src/app/pages/feed/feed.component.ts
@@ -3,9 +3,10 @@ import { TopbarComponent } from '../../components/topbar/topbar.component';
 import { data } from '../../models/data';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
-import { DatePipe } from '@angular/common';
+import { DatePipe, SlicePipe } from '@angular/common';
 import { AvatarModule } from 'primeng/avatar';
 import { TooltipModule } from 'primeng/tooltip';
+import { TagModule } from 'primeng/tag';
 
 @Component({
   selector: 'feed',
@@ -16,6 +17,8 @@ import { TooltipModule } from 'primeng/tooltip';
     DatePipe,
     AvatarModule,
     TooltipModule,
+    TagModule,
+    SlicePipe,
   ],
   templateUrl: './feed.component.html',
   styleUrl: './feed.component.css',
@@ -26,5 +29,9 @@ export class FeedComponent {
   onImageError(event: Event) {
     const target = event.target as HTMLImageElement;
     target.src = 'https://placehold.co/600x400';
+  }
+
+  likeEvent(eventId: String) {
+    console.log(eventId);
   }
 }
