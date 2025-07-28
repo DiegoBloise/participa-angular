33d9dc5d05fa303ddc9499c9ed17425f147d3ffa
Diego Bloise <diego-dev@outlook.com>
2025-07-28 12:33:49 -0300

    added: not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,19 @@
 import { Routes } from '@angular/router';
 import { FeedComponent } from './pages/feed/feed.component';
 import { EventComponent } from './pages/event/event.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: FeedComponent,
   },
-  { path: 'event/:eventId', component: EventComponent },
+  {
+    path: 'event/:eventId',
+    component: EventComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
 ];
